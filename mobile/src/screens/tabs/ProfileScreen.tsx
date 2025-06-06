9efde4102408d412a832cb97910b8f55fc4201b5
Risha0503 <rishaestachia@gmail.com>
2025-06-06 14:32:02 +0200

    profile screen

--- mobile/src/screens/tabs/ProfileScreen.tsx
+++ mobile/src/screens/tabs/ProfileScreen.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 export const ProfileScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Profile</Text>
+
+      <TouchableOpacity style={styles.itemBox} onPress={() => console.log('Settings pressed')}>
+        <Text style={styles.option}>Settings</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.itemBox} onPress={() => console.log('Themes pressed')}>
+        <Text style={styles.option}>Themes</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.itemBox} onPress={() => console.log('Notifications pressed')}>
+        <Text style={styles.option}>Notifications & offers</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -20,5 +32,20 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: '#48BF73',
-  }
-}); 
+    marginBottom: 20,
+  },
+  itemBox: {
+    backgroundColor: '#444',
+    borderRadius: 10,
+    padding: 16,
+    marginBottom: 10,
+    width: '80%',
+    alignItems: 'flex-start',
+  },
+  option: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    textAlign: 'left',
+  },
+});
